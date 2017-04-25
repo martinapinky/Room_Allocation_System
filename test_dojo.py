94fee86613a414468b237ccae7abb391215ffe7f
Martina Alinda <martinaalinda1@gmail.com>
2017-04-25 14:32:17 +0300

    Changes in test_dojo.py

--- test_dojo.py
+++ test_dojo.py
@@ -36,11 +36,9 @@ class TestAddPerson(uniitest.TestCase):
 		number_of_fellows = len(Fellow.get_all_fellows)
 		new_fellow = Dojo.add_person("Martina", "fellow", "Y")
 		new_number_of_fellows = len(Fellow.get_all_fellows)
-		self.assertEqual(number_of_fellows + 1, new_number_of_fellows, )
-		self.assertFalse(new_fellow.livingspace is None)
+		self.assertEqual(number_of_fellows + 1, new_number_of_fellows, msg='New fellow Not Added')
+		self.assertFalse(new_fellow.livingspace is None, msg='New fellow Not allocated livingspace')
 
-	def test_add_staff_successfully(self):
-		number_of_staff = len(Fellow.get_all_staff)
-		new_staff = Dojo.add_person("Maria", "staff")
-		new_number_of_staff = len(Fellow.get_all_staff)
-		self.assertEqual(new_number_of_staff + 1, new_number_of_staff)
+	def test_add_staff_with_accommodation_unsuccessfully(self):
+		new_staff = Dojo.add_person("Maria", "staff", "Y")
+		self.assertEqual(new_staff, "No livingspaces for staff", msg='Staff cannot be allocated livingspace')
